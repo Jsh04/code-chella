d04b910256797f14f33c548150c661e8ab491c49
Jose Silvio <josesilvio.bs@gmail.com>
2023-06-14 13:53:00 -0300

    Banner Finalizado e Conteudos da pagina em desenvolvimento

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -4,18 +4,23 @@ import {  Router } from '@angular/router';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
-  styleUrls: ['./css/principal.component.css', './css/banner/principal-banner.component.css']
+  styleUrls: ['./css/principal.component.css',
+  './css/banner/principal-banner.component.css',
+  './css/card/principal-card.component.css',
+  './css/artistas/principal-artistas.component.css'
+]
 })
 export class PrincipalComponent implements OnInit {
 
   constructor(private route: Router) { }
 
   ngOnInit(): void {
-
+    console.log(this.route.url);
   }
 
   public redirecinarParaIngresso():void{
     this.route.navigate(['/ingresso']);
+
   }
 
 }
